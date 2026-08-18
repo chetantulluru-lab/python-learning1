36cc9be8acfb29019f14bd98b6669426bf311b38
Tulluru Chetan <chetantulluru@gmail.com>
2026-08-19 00:12:13 +0530

    Add basic Python variables and input example

---
+++ Day1.py
@@ -0,0 +1,21 @@
+#day1 of python learning (basic variables and input)
+str="chetan"
+integer=1
+double=10.00
+float=10.0
+print(str)
+print(integer)
+print(double)
+print(float)
+
+#datatypes
+print(type(str))
+print(type(integer))
+print(type(double))
+print(type(float))
+
+#basic program to print name details using input()
+name=str(input("enter name:"))
+age=int(input("enter age:"))
+print(f"person name: {name}")
+print(f"person age: {age}")
